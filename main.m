cec8a284cdc09d59df32ee039bdae50d9885e67c
shaobinqiu <18814122627@163.com>
2019-04-24 15:21:54 +0800

    add import_poscar & write_poscar

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clear
 Lat=[25 0 0
     0 25 0
-    0 0 10];
- name_xyz='/home/qiusb/Desktop/temp/YXB/20190118/T3.xyz';
- path_vasp='/home/qiusb/Desktop/temp/YXB/20190118/';
+    0 0 15];
+ name_xyz='/home/qiusb/Desktop/temp/YXB/graphene_frag/M6.xyz';
+ path_vasp='/home/qiusb/Desktop/temp/YXB/graphene_frag/';
  xyz_to_poscar(Lat, name_xyz,path_vasp)
